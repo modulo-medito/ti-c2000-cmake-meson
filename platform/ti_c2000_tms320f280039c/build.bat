aaa9fa9df85f28ef5e635292425e5b2e1d778a2f
Modulo Medito <chcchc1995@outlook.com>
2024-08-07 11:02:59 +0800

    🐞 fix(freertos):[#]add missed freertos files

--- platform/ti_c2000_tms320f280039c/build.bat
+++ platform/ti_c2000_tms320f280039c/build.bat
@@ -1,8 +1,9 @@
 @echo off
+setlocal EnableDelayedExpansion
 
 @REM Set the absolute directory of the toolchain tools
-set ccs_dir=D:\user_chc\4_app\ccs\2_ws\ti\ccs1271\ccs
-set toolchain_sdk_dir=D:\user_chc\4_app\ccs\2_ws\ti\c2000\C2000Ware_4_02_00_00\device_support\f28003x
+set ccs_dir=C:\ti\ccs1260\ccs
+set toolchain_sdk_dir=C:\ti\c2000\C2000Ware_4_02_00_00\device_support\f28003x
 
 @REM Set the relative path of the toolchain tools
 set toolchain_cpl_dir=%ccs_dir%\tools\compiler\ti-cgt-c2000_22.6.1.LTS
@@ -11,14 +12,15 @@ set hextool=%ccs_dir%\tools\compiler\ti-cgt-c2000_22.6.1.LTS\bin\hex2000.exe
 
 @REM Set the project configurations
 set prjname=minimal_f280039
+set buildpath=build\cmake
 set logfile=build.log
 
 echo ################################################################################
 echo # Pre-build steps                                                              #
 echo ################################################################################
 @REM Remove the build folder
-if exist build rmdir /s /q build
-mkdir build
+if exist %buildpath% rmdir /s /q %buildpath%
+mkdir %buildpath%
 
 echo.
 echo.
@@ -27,19 +29,19 @@ echo ###########################################################################
 echo # Build                                                                        #
 echo ################################################################################
 @REM CMake generate, '2>' means print the stderr and overwrite
-cmake -S ./                                   ^
-    -B ./build                                ^
+cmake -S .\                                   ^
+    -B .\%buildpath%                          ^
     -G "Unix Makefiles"                       ^
     -DCMAKE_MAKE_PROGRAM="%maketool%"         ^
     -DPROJECT_NAME="%prjname%"                ^
     -DTOOLCHAIN_CPL_DIR="%toolchain_cpl_dir%" ^
     -DTOOLCHAIN_SDK_DIR="%toolchain_sdk_dir%" ^
-    2> ./build/%logfile%
+    2> .\%buildpath%\%logfile%
 
 @REM Build, '2>>' means print the stderr and append
-cd build
-REM %maketool% -j 20 all -O 2>> %logfile%
-cmake --build . -j 20
+cd %buildpath%
+%maketool% -j 20 all -O 2>> %logfile%
+REM cmake --build . -j 20
 
 echo.
 echo.
@@ -47,7 +49,7 @@ echo.
 echo ################################################################################
 echo # Post-build steps                                                             #
 echo ################################################################################
-%hextool% --diag_wrap=off ../platform/ti_c2000_tms320f280039c/script/rom_directive/minimal_f280039.hexcmd 2>> %logfile%
+%hextool% --diag_wrap=off ../../platform/ti_c2000_tms320f280039c/script/rom_directive/minimal_f280039.hexcmd 2>> %logfile%
 
 echo.
 echo.
@@ -64,4 +66,5 @@ type %logfile%
 @REM Back to the root
 cd ..
 
+endlocal
 pause
