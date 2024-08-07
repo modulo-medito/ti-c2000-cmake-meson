6c5122191f16a2cf34a06a98c825e023613eb3bb
Modulo Medito <chcchc1995@outlook.com>
2024-08-07 11:13:13 +0800

    🔧 build(batch):[#]update build batch

--- build.bat
+++ build.bat
@@ -1,13 +1,25 @@
 @echo off
+setlocal EnableDelayedExpansion
 
 @REM Set the toolchain
+set buildtool=cmake
 set toolchain=ti_c2000_tms320f280039c
 
 @REM Build
-if exist .\platform\%toolchain%\build.bat (
-    .\platform\%toolchain%\build.bat
-) else (
-    echo Error: build.bat of the specified toolchain is not exist.
+if %buildtool%==cmake (
+    if exist .\platform\%toolchain%\build_cmake.bat (
+        .\platform\%toolchain%\build_cmake.bat
+    ) else (
+        echo Error: build_make.bat for the CMake build tool does not exist.
+    )
+)
+if %buildtool%==meson (
+    if exist .\platform\%toolchain%\build_meson.bat (
+        .\platform\%toolchain%\build_meson.bat
+    ) else (
+        echo Error: build_meson.bat for the Meson build tool does not exist.
+    )
 )
 
+endlocal
 pause
